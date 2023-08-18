29b67d788441f300b5125e7a964e88669673b348
Durga Potnuru <110402074+vijaypotnuru@users.noreply.github.com>
2023-08-18 13:25:46 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all Javascript Code as newer version
+
+// alert(3+3) // we are using node.js, not browser
+
+console.log(3 
+    + 6); // code readability should be high
+console.log("Vijay")
+
+let name = "vijay"
+let age = 18
+
+let isLoggedIn = true
+let state
+
+// number => 2 to the power of 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+
+// object
+
+console.log(typeof(undefined)) // undefined
+console.log(typeof(null)) // object
