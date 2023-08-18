f52d25d1750407ff5b312cafd0f08ae88f675249
Durga Potnuru <110402074+vijaypotnuru@users.noreply.github.com>
2023-08-18 12:56:58 +0000

    01_basics Added

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,22 @@
+const accountId = 454444;
+
+let accountEmail = "vijaypotnuru@gmail.com";
+
+var accountPassword = "12345";
+
+accountCity = "Vizag";
+
+let accountState
+// accountId = 2; // not allowed
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+accountEmail = "vj@vj.com"
+accountPassword = "12315465487"
+accountCity = "Bengaluru"
+
+console.log(accountId)
+console.log({accountId,accountEmail,accountPassword,accountCity,accountState})
